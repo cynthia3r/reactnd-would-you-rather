a5edaeb0c9b841262c3f0536335765861044061a
Sayantani Chaudhuri <sayantani.chaudhuri@gmail.com>
2021-07-13 01:35:53 +1000

    feat: implement action handlers

---
+++ src/actions/answers.js
@@ -0,0 +1,23 @@
+import { showLoading, hideLoading } from 'react-redux-loading-bar';
+import { saveQuestionAnswer} from '../utils/api'
+
+export const ADD_QUESTION_ANSWER = 'ADD_QUESTION_ANSWER';
+
+export function addQuestionAnswer({ authUser, questionId, answer }) {
+    return {
+        type: ADD_QUESTION_ANSWER,
+        authUser,
+        questionId,
+        answer,
+    };
+}
+
+export function handleAddQuestionAnswer(questionAnswer) {
+    return (dispatch) => {
+        dispatch(showLoading());
+
+        return saveQuestionAnswer(questionAnswer)
+            .then(() => dispatch(addQuestionAnswer(questionAnswer)))
+            .then(() => dispatch(hideLoading()));
+    };
+}
--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,8 +1,35 @@
+import { showLoading, hideLoading } from 'react-redux-loading-bar';
+import { saveQuestion } from '../utils/api'
+
 export const RECEIVE_QUESTIONS = 'RECEIVE_QUESTIONS';
+export const ADD_QUESTION = 'ADD_QUESTION';
 
 export function receiveQuestions(questions) {
     return {
         type: RECEIVE_QUESTIONS,
         questions,
+    };
+}
+
+export function addQuestion(question) {
+    return {
+        type: ADD_QUESTION,
+        question,
+    };
+}
+
+export function handleAddQuestion(optionOneText, optionTwoText) {
+    return (dispatch, getState) => {
+        const { authUser } = getState();
+
+        dispatch(showLoading());
+
+        return saveQuestion({
+            optionOneText,
+            optionTwoText,
+            author: authUser
+        })
+            .then((question) => dispatch(addQuestion(question)))
+            .then(dispatch(hideLoading()));
     };
 }
