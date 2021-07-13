f7f9a515337b3b17932ff4164ba21eafc566ffc1
Sayantani Chaudhuri <sayantani.chaudhuri@gmail.com>
2021-07-14 01:08:50 +1000

    feat: implement 404 Error Page component

---
+++ src/components/PageNotFound.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PageNotFound extends Component {
+    render() {
+        return (
+            <div>
+                <h3 className='center'>404 Error: Page Not Found</h3>
+            </div>
+        );
+    }
+}
+
+export default PageNotFound;
