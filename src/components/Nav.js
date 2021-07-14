96bff2b20c5f7c82d2d57571c820a2693153462c
Sayantani Chaudhuri <sayantani.chaudhuri@gmail.com>
2021-07-15 01:42:35 +1000

    feat: displayed user avatar and added nav link

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,27 +1,62 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-export default function Nav() {
-    return (
-        <nav className='nav'>
-            <ul>
-                <li>
-                    <NavLink to='/' exact activeClassName='active'>
-                        Home
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to='/add' exact activeClassName='active'>
-                        New Question
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to='/leaderboard' exact activeClassName='active'>
-                        LeaderBoard
-                    </NavLink>
-                </li>
-            </ul>
-        </nav>
-
-    );
+class Nav extends Component {
+    render() {
+        const { authUser, users } = this.props;
+        return (
+            <nav className='nav'>
+            { authUser ? (
+                <ul>
+                    <li>
+                        <NavLink to='/home' exact activeClassName='active'>
+                            Home
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/add' exact activeClassName='active'>
+                            New Question
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/leaderboard' exact activeClassName='active'>
+                            Leader Board
+                        </NavLink>
+                    </li>
+                    <li>
+                        <span>
+                            <img 
+                                src={users[authUser].avatarURL}
+                                alt={`$users[authUser].avatarURL`}
+                            />
+                        </span>
+                    </li>
+                    <li>
+                        <NavLink to='/logout' exact activeClassName='active'>
+                            Logout
+                        </NavLink>
+                    </li>
+                </ul>
+                ) : (
+                <ul>
+                    <li>
+                        <NavLink to='/' exact activeClassName='active'>
+                            Login
+                        </NavLink>
+                    </li>
+                </ul>
+            )}
+            </nav>
+        );
+    }
 }
+
+function mapStateToProps({ users, authUser }) {
+    return {
+        authUser,
+        users,
+    };
+}
+
+export default connect(mapStateToProps)(Nav);
