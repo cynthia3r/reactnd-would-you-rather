e7bd3cd6fb3db51dd35ce894394c69d01c291d18
Sayantani Chaudhuri <sayantani.chaudhuri@gmail.com>
2021-07-15 01:46:12 +1000

    style: added comments

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -3,6 +3,8 @@ import { getInitialData } from '../utils/api';
 import { receiveUsers } from '../actions/users';
 import { receiveQuestions } from '../actions/questions';
 
+// thunk action creator that gets data from the database and then
+// dispatches actions to the store to set the state
 export function handleInitialData() {
     return (dispatch) => {
         dispatch(showLoading());
