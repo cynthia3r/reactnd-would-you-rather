7dd2e752913ed2094dff0daf4433bd2f408caf21
Sayantani Chaudhuri <sayantani.chaudhuri@gmail.com>
2021-07-15 17:43:12 +1000

    test: wrapped app component inside provider

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,18 @@
 import { render, screen } from '@testing-library/react';
 import App from './components/App';
+import reducer from './reducers'
+import middleware from './middleware'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+
+const store = createStore(reducer, middleware)
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+  const linkElement = screen.getByText(/Would You Rather/i);
   expect(linkElement).toBeInTheDocument();
 });
