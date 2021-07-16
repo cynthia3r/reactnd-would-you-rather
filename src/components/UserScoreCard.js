2bc3e891f5666b8923a57d7559d2a6b0e0afa35e
Sayantani Chaudhuri <sayantani.chaudhuri@gmail.com>
2021-07-16 17:20:03 +1000

    refactor: done minor refactoring for score card design

--- src/components/UserScoreCard.js
+++ src/components/UserScoreCard.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Card from 'react-bootstrap/Card';
-
-
 class UserScoreCard extends Component {
     render() {
         const { user } = this.props;
         const { name, avatarURL, answers, questions } = user;
 
-        return (
+        return ( 
             <Card>
                 <Card.Header>
                     <span>
@@ -19,17 +17,16 @@ class UserScoreCard extends Component {
                         />
                     </span>
                     {name}
+                    <br />
+                    <b>Score: {Object.keys(answers).length + questions.length}</b>
                 </Card.Header>
                 <Card.Body>
-                    <b>Score: {Object.keys(answers).length + questions.length}</b>   
-                </Card.Body>
-                <Card.Footer>
                     <Card.Text>
                         Answered Questions: {Object.keys(answers).length}
                         <br />
                         Created Questions: {questions.length}
-                    </Card.Text>
-                </Card.Footer>
+                    </Card.Text>   
+                </Card.Body>
             </Card>
         );
     }
