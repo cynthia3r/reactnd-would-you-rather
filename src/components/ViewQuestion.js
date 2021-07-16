1fb448bbb44692ab4d8a08542b5ade1a2fa49bf0
Sayantani Chaudhuri <sayantani.chaudhuri@gmail.com>
2021-07-16 17:33:36 +1000

    feat: implement functionality to display poll question details

---
+++ src/components/ViewQuestion.js
@@ -0,0 +1,30 @@
+import React, { Component, Fragment } from 'react';
+import { connect } from 'react-redux';
+import SubmitVote from './SubmitVote';
+import ViewPollResult from './ViewPollResult';
+
+class ViewQuestion extends Component {
+    render() {
+        const { userAnswers, match } = this.props;
+        const questionId = match.params.id;
+        const answered = userAnswers.hasOwnProperty(questionId) ? true : false;
+
+        return (
+            <Fragment>
+                {answered ? 
+                    <ViewPollResult key={questionId} questionId={questionId} /> : 
+                    <SubmitVote key={questionId} questionId={questionId} /> }
+            </Fragment>
+        );
+    }
+}
+
+function mapStateToProps({ authUser, users, questions }) {
+    const userAnswers = users[authUser].answers;
+
+    return {
+        userAnswers,
+    };
+}
+
+export default connect(mapStateToProps)(ViewQuestion);
