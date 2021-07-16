00c61920368cc7f9f576b3a5bf433da767e4ac0b
Sayantani Chaudhuri <sayantani.chaudhuri@gmail.com>
2021-07-16 17:02:26 +1000

    style: import react bootstrap css file

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
 import App from './components/App'
 import reducer from './reducers'
 import middleware from './middleware'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css'
 
 const store = createStore(reducer, middleware)
 
